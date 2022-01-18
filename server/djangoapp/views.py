8d0d0fc710efa5eb0d7852357a0c68f14fd28b81
AF20181 <armin.fuhrmann@organon-is.de>
2022-01-18 10:58:32 +0100

    name

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -119,7 +119,7 @@ def add_review(request, dealer_id):
                 review["purchase"] = bool(True)
             review["purchase_date"] = datetime.strptime(form.get("purchase_date"), "%d/%m/%Y").isoformat()
             car = models.CarModel.objects.get(pk=form["car"])
-            review["car_make"] = car.carmake
+            review["car_make"] = car.carmake.name
             review["car_model"] = car.name
             review["car_year"] = int(car.year.strftime("%Y"))
             json_payload = {}
